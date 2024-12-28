bbad6bacf8e9559bc0ea768069b9caf066148552
whatisaProCoder <debnathpritam2005@gmail.com>
2024-12-28 17:18:47 +0000

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = 0;
+  arr.forEach((element) => sum += element);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let mult = 1;
+  arr.forEach(element => mult *= element);
+  return mult;
 };
 
-const power = function() {
-	
+const power = function (n, p) {
+  return Math.pow(n, p);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let f = 1;
+  for (let i = 2; i <= n; i++)
+    f *= i;
+  return f;
 };
 
 // Do not edit below this line
