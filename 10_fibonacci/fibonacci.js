5ea352b5cc6336e33817fe96660ffff9008a90fe
whatisaProCoder <debnathpritam2005@gmail.com>
2024-12-28 17:38:06 +0000

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+    if (typeof n == 'string')
+        n = Number.parseInt(n)
+    if (n < 0)
+        return 'OOPS';
+    a = 0;
+    b = 1;
+    for (let i = 0; i < n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return a;
 };
 
 // Do not edit below this line
