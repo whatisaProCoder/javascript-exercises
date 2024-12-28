5214967e4a94d0115521f9e19993a8177508facc
whatisaProCoder <debnathpritam2005@gmail.com>
2024-12-28 17:07:55 +0000

    tempconv

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (f) {
+  let c = (5 * (f - 32)) / 9;
+  c = Math.round(c * 10) / 10;
+  return c;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (c) {
+  let f = (9 * c) / 5 + 32;
+  f = Math.round(f * 10) / 10;
+  return f;
 };
 
 // Do not edit below this line
