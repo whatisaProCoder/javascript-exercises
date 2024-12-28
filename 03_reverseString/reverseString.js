c9e78d9194090b0c5f0fe2cecdfd57cbbb195eac
whatisaProCoder <debnathpritam2005@gmail.com>
2024-12-28 13:35:17 +0000

    reversestring

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    string = string.split('')
+    let l = 0
+    let r = string.length
+    while (l < r) {
+        [string[l], string[r]] = [string[r], string[l]]
+        l++
+        r--
+    }
+    return string.join('')
 };
 
 // Do not edit below this line
