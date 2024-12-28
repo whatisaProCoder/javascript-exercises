85b6a1a96d36a1752ab29d8139066a9763913d0f
whatisaProCoder <debnathpritam2005@gmail.com>
2024-12-28 16:40:11 +0000

    sumall

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (x, y) {
+    // code the edge cases
+    if (!Number.isInteger(x) || !Number.isInteger(y)) {
+        return 'ERROR';
+    }
+    if (x < 0 || y < 0) {
+        return 'ERROR';
+    }
+    if (x > y) {
+        [x, y] = [y, x];
+    }
+    let sum = 0;
+    for (let i = x; i <= y; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
