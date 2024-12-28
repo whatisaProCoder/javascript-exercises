28979447d6b569a39e2d06414cf23c151a38a5b8
whatisaProCoder <debnathpritam2005@gmail.com>
2024-12-28 17:31:27 +0000

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+const isalnum = function (ch) {
+    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9'))
+        return true;
+    return false;
+};
+const palindromes = function (str) {
+    let l = 0;
+    let r = str.length - 1;
+    while (l < r) {
+        while (l < r && !isalnum(str[l]))
+            l++;
+        while (l < r && !isalnum(str[r]))
+            r--;
+        if (str[l].toLowerCase() != str[r].toLowerCase())
+            return false;
+        l++;
+        r--;
+    }
+    return true;
 };
 
 // Do not edit below this line
