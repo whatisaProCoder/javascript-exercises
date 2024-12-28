c4c962a00bcabf2bef85b0bc08c1aa1ff7fc29c8
whatisaProCoder <debnathpritam2005@gmail.com>
2024-12-28 14:04:21 +0000

    removefromarray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (array, ...itemsToRemove) {
+    return array.filter((item) => !itemsToRemove.includes(item))
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
