41c04de551c60906bc686583fb572797fa6afad7
whatisaProCoder <debnathpritam2005@gmail.com>
2024-12-28 18:24:00 +0000

    titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    const titles = [];
+    books.forEach((book) => titles.push(book.title));
+    return titles;
 };
 
 // Do not edit below this line
