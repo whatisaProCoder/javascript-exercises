f79d77d7fce956e691edb8a0a5d69d25241f30f0
whatisaProCoder <debnathpritam2005@gmail.com>
2025-02-02 14:12:51 +0000

    finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const getAge = (birth, death) => {
+    if(!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+};
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentPersonAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge > currentPersonAge?  oldest : currentPerson;
+    }, {});
 };
 
 // Do not edit below this line
